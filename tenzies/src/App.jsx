097a68e6e58a777052559200aaf73dbd8e34fbc7
Anri20 <88647934+Anri20@users.noreply.github.com>
2025-03-16 19:23:41 +0700

    Add Dice
    Generate Dice with random value

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,8 +1,24 @@
 import React from "react"
+import Die from "./components/Die"
 
 export default function App() {
+    const [dice, setDice] = React.useState(() => generateAllNewDice())
+
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6)
+            }))
+    }
+
+    const allDice = dice.map(die => <Die value={die.value} />)
+
     return (
         <main>
+            <div className="dice-container">
+                {allDice}
+            </div>
         </main>
     )
 }
